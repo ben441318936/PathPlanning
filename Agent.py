d4e9f413b913b975c3a6bff26615c4c81b766ede
Jingbin <ben441318936@gmail.com>
2022-04-13 20:39:12 -0700

    added basic planner

--- Agent.py
+++ Agent.py
@@ -1,17 +1,20 @@
 import numpy as np
+from pqdict import pqdict
+
+from Grid import Grid, Direction, ScanStatus
 
 class Agent(object):
     '''
     Agent has a map that updates as it explores.
-    Each grid cell can be empty, occupied by wall, or has an object in it.
-        Out-of-bounds: -1
-        Empty: 0
-        Occupied: 1
-        HasTarget: 2
     '''
     def __init__(self, map_size) -> None:
-        self._map = np.zeros((map_size),dtype=int) # assume everything empty
-        self._pos = np.array([map_size[0]//2, map_size[1]//2]) # agent initializes to center of map
+        self._map = np.zeros((map_size), dtype=int) # assume everything empty
+        self._pos = np.array([map_size[0]//2, map_size[1]//2], dtype=int) # agent initializes to center of map
+        self._target = None
+        self._path = None
+
+    def print_map(self) -> None:
+        print(self._map)
 
     def cone_of_vision(self) -> list:
         '''
@@ -24,23 +27,173 @@ class Agent(object):
         area = [np.array([i,j]) for j in range(-1,2) for i in range(-1,2)]
         return area
 
-    def move(self,dir) -> None:
-        if dir == 0: # right
-            self._pos += np.array([0,1])
-        elif dir == 1: # down
-            self._pos += np.array([1,0])
-        elif dir == 2: # left
-            self._pos += np.array([0,-1])
-        elif dir == 3: # up
-            self._pos += np.array([-1,0])
+    def move(self, dir, grid:Grid=None) -> bool:
+        if grid is None or grid.agent_move(dir):
+            if dir == Direction.RIGHT: # right
+                self._pos += np.array([0,1])
+            elif dir == Direction.DOWN: # down
+                self._pos += np.array([1,0])
+            elif dir == Direction.LEFT: # left
+                self._pos += np.array([0,-1])
+            elif dir == Direction.UP: # up
+                self._pos += np.array([-1,0])
+            return True
+        else:
+            return False
 
     def update_map(self, scan_result) -> None:
         for res in scan_result:
             coord = self._pos + res[0]
             if not (coord[0] < 0 or coord[0] >= self._map.shape[0] or coord[1] < 0 or coord[1] >= self._map.shape[1]):
-                self._map[coord[0],coord[1]] = res[1]
+                if res[1] == ScanStatus.WALL or res[1] == ScanStatus.OBSTRUCTED:
+                    self._map[coord[0],coord[1]] = ScanStatus.WALL
+                elif res[1] == ScanStatus.TARGET or res[1] == ScanStatus.BOTH:
+                    self._map[coord[0],coord[1]] = ScanStatus.TARGET
+                else:
+                    self._map[coord[0],coord[1]] = ScanStatus.EMPTY
 
-    def print_map(self) -> None:
-        print(self._map)
+    def set_target(self, target_pos) -> None:
+        '''
+        Set target position relative to the current agent position.
+        After setting, self._map[self._target[0], self._target[1]] should be where the target is
+        '''
+        self._target = self._pos + target_pos
+        while self._target[0] < 0 or self._target[0] >= self._map.shape[0] or self._target[1] < 0 or self._target[1] >= self._map.shape[1]:
+            self.expand_map()
+            self._target = self._pos + target_pos
 
+    def expand_map(self, factor=2) -> None:
+        '''
+        Used when the current map is too small.
+        '''
+        new_shape = (int(self._map.shape[0]*factor), int(self._map.shape[1]*factor))
+        pad_widths = np.array([(new_shape[0]-self._map.shape[0])//2, (new_shape[1]-self._map.shape[1])//2])
+        self._map = np.pad(self._map, ((pad_widths[0], pad_widths[0]), (pad_widths[1], pad_widths[1])), constant_values=ScanStatus.EMPTY)
+        self._pos = self._pos + pad_widths
 
+    def _get_neighbors_inds(self, parent_ind) -> list:
+        result = []
+        parent_pos = np.unravel_index(parent_ind, self._map.shape)
+        for i in range(-1,2):
+            for j in range(-1,2):
+                pos = parent_pos + np.array([i,j])
+                if (not (pos[0] == 0 and pos[1] == 0)) and (i == 0 or j == 0) and (pos[0] >= 0 and pos[0] < self._map.shape[0]) and (pos[1] >= 0 and pos[1] < self._map.shape[1]):
+                    if self._map[pos[0], pos[1]] != ScanStatus.WALL:
+                        result.append(np.ravel_multi_index(pos, self._map.shape))
+        return result
+
+    def _get_parent_children_costs(self, parent_ind) -> list:
+        result = []
+        parent_pos = np.unravel_index(parent_ind, self._map.shape)
+        for i in range(-1,2):
+            for j in range(-1,2):
+                pos = parent_pos + np.array([i,j])
+                if (not (pos[0] == 0 and pos[1] == 0)) and (i == 0 or j == 0) and (pos[0] >= 0 and pos[0] < self._map.shape[0]) and (pos[1] >= 0 and pos[1] < self._map.shape[1]):
+                    if self._map[pos[0], pos[1]] != ScanStatus.WALL:
+                        result.append(1)
+        return result
+    
+    def plan(self) -> bool:
+        '''
+        Assume that the current map is correct, plan a path to the target.
+        Using weighted A* with Euclidean distance as heuristic.
+        This heuristic is consistent for all k-connected grid.
+        '''
+        eps = 1
+        # Initialize the data structures
+        # Labels
+        g = np.ones((self._map.shape[0] * self._map.shape[1])) * np.inf
+        start_ind = np.ravel_multi_index(self._pos, self._map.shape)
+        target_ind = np.ravel_multi_index(self._target, self._map.shape)
+        g[start_ind] = 0
+        # Priority queue for OPEN list
+        OPEN = pqdict({})
+        OPEN[start_ind] = g[start_ind] + eps * np.linalg.norm(self._pos - self._target)
+        # A regular list for CLOSED list
+        CLOSED = []
+        # Predecessor list to keep track of path
+        pred = -np.ones((self._map.shape[0] * self._map.shape[1])).astype(int)
+
+        done = False
+
+        while not done:
+            if len(OPEN) != 0 :
+                parent_ind = OPEN.popitem()[0]
+            else:
+                break
+            CLOSED.append(parent_ind)
+            if parent_ind == target_ind:
+                done = True
+                break
+            # Get list of children
+            children_inds = self._get_neighbors_inds(parent_ind)
+            # Get list of costs from parent to children
+            # for a 4-connected grid, cost is 1 for all
+            children_costs = self._get_parent_children_costs(parent_ind)
+            for j in range(len(children_inds)):
+                child_ind = children_inds[j]
+                if (not child_ind == -1) and (not child_ind in CLOSED):
+                    if g[child_ind] > g[parent_ind] + children_costs[j]:
+                        g[child_ind] = g[parent_ind] + children_costs[j]
+                        pred[child_ind] = parent_ind
+                        # This updates if child already in OPEN
+                        # and appends to OPEN otherwise
+                        child_pos = np.unravel_index(child_ind, self._map.shape)
+                        OPEN[child_ind] = g[child_ind] + eps * np.linalg.norm(child_pos - self._target)
+
+        # We have found a path
+        path = []
+        if done:
+            ind = target_ind
+            while True:
+                pos = np.unravel_index(ind, self._map.shape)
+                path.append(pos)
+                if ind == start_ind:
+                    break
+                else:
+                    ind = pred[ind]
+        path = list(reversed(path))
+        self._path = np.array(path)
+        return done
+
+    def get_path_agent_frame(self) -> np.ndarray:
+        if self._path is not None:
+            return self._path - self._pos
+        else:
+            return np.array([])
+
+    
+
+if __name__ == "__main__":
+    G = Grid((10,10))
+    
+    G.place_agent(8,8)
+    G.place_target(0,4)
+
+    A = Agent((5,5))
+    A.set_target(G.relative_target_pos())
+    A._map[7,2:10] = ScanStatus.WALL
+    print(A._pos)
+    print(A._target)
+    print(A._map)
+
+    # print("Initial map")
+    # A.print_map()
+    # A.update_map(G.scan(A.cone_of_vision()))
+    # print("After scan")
+    # A.print_map()
+
+    # A.move(Direction.UP, grid=G)
+    # A.update_map(G.scan(A.cone_of_vision()))
+    # print("After move and scan")
+    # A.print_map()
+
+    # A.move(Direction.UP, grid=G)
+    # A.update_map(G.scan(A.cone_of_vision()))
+    # print("After move and scan")
+    # A.print_map()
+
+    planning_status = A.plan()
+    print("Path found:", planning_status)
+    print("Path in agent frame: \n", A.get_path_agent_frame())
+    print("Path in world frame: \n", A.get_path_agent_frame() + G.agent_pos)
--- Grid.py
+++ Grid.py
@@ -1,54 +1,76 @@
 import numpy as np
+from enum import IntEnum
+
+class GridStatus(IntEnum):
+    EMPTY = 0
+    WALL = 1
+    TARGET = 2
+    AGENT = 3
+    BOTH = 4
+
+class ScanStatus(IntEnum):
+    OUT_OF_BOUNDS = -2
+    OBSTRUCTED = -1
+    EMPTY = GridStatus.EMPTY
+    WALL = GridStatus.WALL
+    TARGET = GridStatus.TARGET
+    AGENT = GridStatus.AGENT
+    BOTH = GridStatus.BOTH
+
+class Direction(IntEnum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 class Grid(object):
     '''
     Grid that represents the environment.
     Core data structure is a 2D array.
-    Each grid cell can be empty, occupied by wall, or has an object in it.
-        Empty: 0
-        Occupied: 1
-        HasTarget: 2
-        HasAgent: 3
-        HasBoth: 4
     '''
-
     def __init__(self,grid_size) -> None:
         self._grid = np.zeros((grid_size),dtype=int)
-        self.agent_pos = np.array([0,0])
-        self.target_pos = np.array([0,0])
-        self.place_agent(0,0)
-        self.place_target(5,5)
+        self.agent_pos = None
+        self.target_pos = None
 
-    def place_agent(self,row,col) -> None:
-        if self._grid[row,col] != 1:
-            if self._grid[row,col] == 2:
-                self._grid[row,col] = 4
+    def place_agent(self,row,col) -> bool:
+        if row >= 0 and row < self._grid.shape[0] and col >= 0 and col < self._grid.shape[1] and self._grid[row,col] != GridStatus.WALL:
+            if self.agent_pos is not None:
+                self._grid[self.agent_pos[0], self.agent_pos[1]] = 0
+            if self._grid[row,col] == GridStatus.TARGET:
+                self._grid[row,col] = GridStatus.BOTH
             else:
-                self._grid[row,col] = 3
+                self._grid[row,col] = GridStatus.AGENT
             self.agent_pos = np.array([row,col])
+            return True
+        else:
+            return False
 
     def place_target(self,row,col) -> None:
-        if self._grid[row,col] != 1:
-            if self._grid[row,col] == 3:
-                self._grid[row,col] = 4
+        if row >= 0 and row < self._grid.shape[0] and col >= 0 and col < self._grid.shape[1] and self._grid[row,col] != GridStatus.WALL:
+            if self.target_pos is not None:
+                self._grid[self.target_pos[0], self.target_pos[1]] = 0
+            if self._grid[row,col] == GridStatus.AGENT:
+                self._grid[row,col] = GridStatus.BOTH
             else:
-                self._grid[row,col] = 2
+                self._grid[row,col] = GridStatus.TARGET
             self.target_pos = np.array([row,col])
+            return True
+        else:
+            return False
 
     def agent_move(self,dir) -> bool:
-        if dir == 0: # right
+        if dir == Direction.RIGHT: # right
             coord = self.agent_pos + np.array([0,1])
-        elif dir == 1: # down
+        elif dir == Direction.DOWN: # down
             coord = self.agent_pos + np.array([1,0])
-        elif dir == 2: # left
+        elif dir == Direction.LEFT: # left
             coord = self.agent_pos + np.array([0,-1])
-        elif dir == 3: # up
+        elif dir == Direction.UP: # up
             coord = self.agent_pos + np.array([-1,0])
 
         if self.not_wall(coord):
-            self._grid[self.agent_pos[0],self.agent_pos[1]] = 0
-            self.place_agent(coord[0],coord[1])
-            return True
+            return self.place_agent(coord[0], coord[1])
         else:
             return False
 
@@ -64,11 +86,33 @@ class Grid(object):
         for offset in area:
             coord = self.agent_pos + offset
             if coord[0] < 0 or coord[0] >= self._grid.shape[0] or coord[1] < 0 or coord[1] >= self._grid.shape[1]:
-                result.append((offset, -1))
-            else:
-                result.append((offset, self._grid[coord[0],coord[1]]))
+                result.append((offset, ScanStatus.OUT_OF_BOUNDS))
+            elif self._grid[coord[0],coord[1]] == GridStatus.EMPTY or self._grid[coord[0],coord[1]] == GridStatus.AGENT:
+                result.append((offset, ScanStatus.EMPTY))
+            elif self._grid[coord[0],coord[1]] == GridStatus.WALL:
+                result.append((offset, ScanStatus.WALL))
+            elif self._grid[coord[0],coord[1]] == GridStatus.TARGET or self._grid[coord[0],coord[1]] == GridStatus.BOTH:
+                result.append((offset, ScanStatus.TARGET))
         return result
 
+    def relative_target_pos(self) -> None:
+        '''
+        Returns the position of the target relative to the agent.
+        '''
+        return self.target_pos - self.agent_pos
 
     def print_grid(self) -> None:
         print(self._grid)
+
+
+if __name__ == "__main__":
+    G = Grid((10,10))
+    print("Initialization")
+    G.print_grid()
+    G.place_agent(3,3)
+    G.place_target(2,2)
+    print("After placement")
+    G.print_grid()
+    G.agent_move(Direction.UP)
+    print("After agent move")
+    G.print_grid()
